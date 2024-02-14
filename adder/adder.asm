4a7c5269c404a9d517d34f9cb9efa4072b72463c
Lomzem <lawjaylee1@icloud.com>
2024-02-14 11:36:43 -0800

    more work on adder

--- adder/adder.asm
+++ adder/adder.asm
@@ -7,15 +7,29 @@
 ; Project Description: Receives integer input from user and adds them
 ;*************************************************************************
 
+; For storing data with a starting value
 section .data
+; Create prompt variable, allocate db for string, 0Ah for newline
+prompt db "Enter a number between 1 and 9, or enter 0 to exit", 0Ah
+
+; const variable to store len of prompt string
+; gets current memory location - prompt location
+prompt_len equ $-prompt
 
 ; For storing data with no starting value
 section .bss
 input resb 1; reserve one byte for input variable
+total resb 1; reserve on byte for total variable
 
 section .text
 global _start:
 _start:
+    ; Prompt user to enter input number between 1 and 9, or 0 to exit
+    mov eax, 4; sys_write
+    mov ebx, 1; write to stdout
+    mov ecx, prompt; get what to write from prompt variable
+    mov edx, prompt_len; get how many bytes to write from len variable
+    ; mov ecx, 
 
     ; End program
     mov eax, 1; sys_exit
